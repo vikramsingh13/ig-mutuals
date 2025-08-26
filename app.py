be2b3e96f71353d1a0ec8abd5764e8b0e784af9f
Vikram Singh <vikramsingh.webdev@gmail.com>
2025-08-26 04:59:13 +0000

    returns non-mutual list

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import re
 pattern = re.compile(r'alt="([^"]+?)\'s profile picture"')
 
 def extract_names(path):
+    # using sets for faster comparison later on
     names = set()
     with open(path, "r", encoding="utf-8", errors="ignore") as f:
         for line in f:
@@ -14,11 +15,21 @@ def extract_names(path):
                 names.add(m.group(1))
     return names
 
+def create_a_not_in_b_list(a, b):
+    """a is set of following. b is set of followers. return list of followings not in followers"""
+    return [name for name in a if name not in b]
+
 followers = extract_names("followers.txt")
 following = extract_names("following.txt")
+
+non_mutuals = create_a_not_in_b_list(following, followers)
 
 # Example usage:
 print(f"followers: {len(followers)}")
 print(f"following: {len(following)}")
 # print(followers)
 # print(following)
+
+print(f"Non-mutuals: {non_mutuals}")
+
+
